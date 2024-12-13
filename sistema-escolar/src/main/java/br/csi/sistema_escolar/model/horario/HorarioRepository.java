256c035d4448338e6668c078931386ac1ba5e671
Nicolas Meurer <110570873+Fenn3kk@users.noreply.github.com>
2024-12-13 17:06:08 +0000

    commit3

--- sistema-escolar/src/main/java/br/csi/sistema_escolar/model/horario/HorarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.csi.sistema_escolar.model.horario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface HorarioRepository extends JpaRepository<Horario, Long> {
-    public Horario findHorarioByUuid(UUID uuid);
-    public void deleteHorarioByUuid(UUID uuid);
-}
